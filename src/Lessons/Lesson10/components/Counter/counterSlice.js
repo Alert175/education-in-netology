ac4561a9aa4167fd1ee150b53d69ba09086aca4a
Alert175 <ilya290897@gmail.com>
2021-05-20 23:13:38 +0300

    education redux / redux-toolkit

---
+++ src/Lessons/Lesson10/components/Counter/counterSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+// создание начального значения среза глобального хранилища
+const initialState = {
+  value: 0,
+};
+
+// созданеи и экспорт среза глобального хранилища
+export const slice = createSlice({
+  // название среза
+  name: 'counter',
+  // объявление начального состояния
+  initialState,
+  // объект со всеми редъюсерами (методов управления состоянием среза глобального хранилища)
+  reducers: {
+    // метод изменения состояния среза ГХ
+    increment: (state) => {
+      state.value += 1;
+    },
+    // метод изменения состояния среза ГХ с полезной нагрузкой
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+// экспорт экшенов (методов управления состоянием среза ГХ)
+export const { increment, incrementByAmount } = slice.actions;
+
+// экспорт селектора состояния среза ГХ
+export const selectCount = (state) => state.counter.value;
+
+export default slice.reducer;
